c5bf129b3f77283724d48667f4087bd674334fac
Den <no@>
2024-04-28 18:35:15 +0300

    Добавление капитана

---
+++ data/jobs.py
@@ -0,0 +1,19 @@
+import datetime
+import sqlalchemy
+from sqlalchemy import orm
+from .db_session import SqlAlchemyBase
+
+
+class Jobs(SqlAlchemyBase):
+    __tablename__ = 'Jobs'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    team_leader = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('users.id'))
+    job = sqlalchemy.Column(sqlalchemy.String)
+    work_size = sqlalchemy.Column(sqlalchemy.Integer)
+    collaborators = sqlalchemy.Column(sqlalchemy.String)
+    start_date = sqlalchemy.Column(sqlalchemy.Date)
+    finish_date = sqlalchemy.Column(sqlalchemy.Date)
+    is_finished = sqlalchemy.Column(sqlalchemy.Boolean)
+    user = orm.relationship('User')
